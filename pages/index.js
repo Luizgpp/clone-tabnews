38df7982eed039f8203b416a53261e3c38d22d6c
Luiz Gabriel Parreira Pereira <luizgpp@gmail.com>
2024-01-03 16:42:29 +0000

    Teoria McDonalds

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
 function Home() {
-  return <h1>Casa comigo? Se sim, me da um beijo!</h1>;
+  return (
+    <>
+      <h1>Teoria McDonalds</h1>
+      <ul>
+        <li>Pessoas com interesse em games</li>
+        <li>Desenvolvimendo de Games</li>
+        <li>eSports</li>
+        <li>Notigias do mundo de Games</li>
+        <li>Discuções para possiveis no games/tipo de games</li>
+      </ul>
+      <a href="https://papodehomem.com.br/teoria-mcdonalds/">
+        Teoria McDonalds
+      </a>
+    </>
+  );
 }
 
 export default Home;
