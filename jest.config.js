263214334de8901df35938055dc382e5407fee4c
Luiz Gabriel Parreira Pereira <luizgpp@gmail.com>
2024-12-05 01:28:56 +0000

    adds `/api/v1/migrations` endpoint

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+const nextJest = require("next/jest");
+const createJestConfig = nextJest({ dir: "." });
+const jestConfig = createJestConfig({
+  moduleDirectories: ["node_modules", "<rootDir>"],
+});
+
+module.exports = jestConfig;
